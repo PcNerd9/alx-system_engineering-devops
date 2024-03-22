bdc0acd828c593f9a47d776619dbd8364cecc368
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-22 18:31:36 +0100

    ensure to installed requirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,6 +4,11 @@ package { 'pip3':
   ensure => installed,
   name   => 'pip3',
 }
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
