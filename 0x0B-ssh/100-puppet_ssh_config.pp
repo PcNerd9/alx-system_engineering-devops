185a5d0af7591639a6cc1f07b2b72871a92790d5
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-03-26 13:56:11 +0100

    resolve requirement issues

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,17 @@
 # configure the /etc/ssh/ssh_config
 
-file {'/etc/ssh/ssh_config':
+file {'/etc/ssh/sshd_config':
   ensure => present,
 }
 
 file_line {'modify_passwordauthentication':
-  path    => '/etc/ssh/ssh_config',
+  path    => '/etc/ssh/sshd_config',
   line => 'PasswordAuthentication no',
   match => '^PasswordAuthentication yes',
 }
 
 file_line {'modify_identify_key':
-  path => '/etc/ssh/ssh_config',
+  path => '/etc/ssh/sshd_config',
   line => 'IdentityFile ~/.ssh/school',
   match => '^IdentityFile ~/ssh/id_rsa',
 }
