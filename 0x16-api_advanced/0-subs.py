5ab70aa5167bc490ebb2c783871e4c3c395e745b
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-06-19 23:27:29 +0100

    resolve requirement issues in the 0-sub.py script v3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-
 """
-queries the Reddit API and returns the number of subscribers
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
-
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    query the Reddit API with the subreddit parameter
-    """
-    url = f"https://www.reddit.com/subreddits/search.json?q={subreddit}"
-    response = requests.get(url, allow_redirects=False)
-    if (response.status_code != 200):
-        return 0
-    subscriber = 0
-    json_data = response.json()
-    children = json_data['data']['children']
-    for child in children:
-        if (child['data']['title'] == subreddit):
-            subscriber = child['data']['subscribers']
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    return (int(subscriber))
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
