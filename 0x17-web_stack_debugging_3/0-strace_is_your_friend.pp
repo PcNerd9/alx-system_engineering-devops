b4bc184d0201bb2da759a3fd1d63e04ea05df0d6
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-06-20 20:21:13 +0100

    debugged a apache web server

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,21 @@
+# Ensure the Apache log directory exists and has the correct permissions
+
+file { '/var/log/apache2':
+  ensure  => 'directory',
+  owner   => 'www-data',   # Adjust this depending on your Apache user
+  group   => 'www-data',   # Adjust this depending on your Apache group
+  mode    => '0755',
+}
+
+# Ensure Apache service is running
+service { 'apache2':
+  ensure     => 'running',
+  enable     => true,
+  subscribe  => File['/var/log/apache2'], # Restart Apache if the directory is changed
+}
+
+# Optionally, ensure Apache is installed (if not already done)
+package { 'apache2':
+  ensure => 'installed',
+}
+
