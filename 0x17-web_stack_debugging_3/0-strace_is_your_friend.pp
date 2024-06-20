df41692d2ce82c8bc51b4c311e53c7eb95cbfeff
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-06-20 20:26:39 +0100

    resolve puppet-lint requirement issues

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,17 @@
 # Ensure the Apache log directory exists and has the correct permissions
 
 file { '/var/log/apache2':
-  ensure  => 'directory',
-  owner   => 'www-data',   # Adjust this depending on your Apache user
-  group   => 'www-data',   # Adjust this depending on your Apache group
-  mode    => '0755',
+  ensure => 'directory',
+  owner  => 'www-data',   # Adjust this depending on your Apache user
+  group  => 'www-data',   # Adjust this depending on your Apache group
+  mode   => '0755',
 }
 
 # Ensure Apache service is running
 service { 'apache2':
-  ensure     => 'running',
-  enable     => true,
-  subscribe  => File['/var/log/apache2'], # Restart Apache if the directory is changed
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/var/log/apache2'], # Restart Apache if the directory is changed
 }
 
 # Optionally, ensure Apache is installed (if not already done)
